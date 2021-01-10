e0443b6e310e12e3b1fddc26c7b07b163112bc3c
Illya <illya123456@outlook.com>
2021-01-10 13:49:35 +0200

    Fixed homework 2

--- homework 2/hw2.py
+++ homework 2/hw2.py
@@ -3,7 +3,8 @@ from random import randint
 from datetime import date
 from threading import Lock
 
-HOUSES = []
+HOUSES = set()
+REALTORS = set()
 
 
 class House:
@@ -77,7 +78,9 @@ class Human(ABC):
             raise Exception("Human hasn't enough money")
         self.money -= house.cost
         self.buildings.add(house)
-        HOUSES.remove(house)
+        HOUSES.discard(house)
+        for r in REALTORS:
+            r.houses_for_sell.discard(house)
 
     # for inheritance and presents
     def add_house(self, house):
@@ -129,13 +132,14 @@ class Realtor(Human):
     def sell_house(self, house: House, person: Person):
         salary = 0
         # steal money with 10% chance
-        if not randint(1, 101) < 10:
+        if randint(1, 101) < 10:
             salary += house.cost * 0.05
 
         salary += house.cost * 0.05
         person.money -= house.cost + salary
         self.make_money(salary)
-        self.houses_for_sell.remove(house)
+        HOUSES.discard(house)
+        self.houses_for_sell.discard(house)
 
     def get_discounts(self):
         return [house.discount for house in self.houses_for_sell]
@@ -151,13 +155,14 @@ if __name__ == '__main__':
     realtor = Realtor(name='Ivan',
                       birth_date=date(year=1998, day=11, month=11),
                       money=999999)
+    REALTORS.add(realtor)
     person1 = Person(name='Irma',
                      birth_date=date(year=1998, day=11, month=11),
                      money=999999)
     person2 = Person(name='Alex',
                      birth_date=date(year=1998, day=11, month=11),
                      money=99999)
-    HOUSES = [House(99999, 45),
-              House(9999, 46),
-              House(9999, 48),
-              House(9999, 90)]
+    HOUSES.add(House(99999, 45))
+    HOUSES.add(House(9999, 46))
+    HOUSES.add(House(9999, 48))
+    HOUSES.add(House(9999, 90))
