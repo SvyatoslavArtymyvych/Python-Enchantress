94e5db06ca602dd0ec63ffd249e9647ee45cecaf
SvyatoslavArtymyvych <p0wer.bla4er2014@gmail.com>
2021-01-16 15:43:35 +0200

    completed hw4

--- lectures/tests/hen_house/test_hen_class.py
+++ lectures/tests/hen_house/test_hen_class.py
@@ -1,64 +1,67 @@
 import unittest
 from unittest.mock import patch
-from lectures.tests.hen_house.hen_class import HenHouse, ErrorTimesOfYear
+from hen_class import HenHouse, ErrorTimesOfYear
 
 
 class TestHenHouse(unittest.TestCase):
 
     def setUp(self) -> None:
-        # optional method, may be used to initialize hen_house instance
-        pass
+        self.hen_house = HenHouse(15)
 
     def test_init_with_less_than_min(self):
-        # initialize HenHouse with hens_count less than HenHouse.min_hens_accepted
-        # make sure error is raised
-        pass
+        with self.assertRaises(ValueError) as e:
+            self.hen_house = HenHouse(3)
 
     def test_season(self):
-        # mock the datetime method/attribute which returns month number
-        # make sure correct month ("winter"/"spring" etc.) is returned from season method
-        # try to return different seasons
-        pass
+        with patch('hen_class.datetime.datetime') as e:
+            e.today().month = 1
+            self.assertEqual(self.hen_house.season, 'winter')
+
+            e.today().month = 9
+            self.assertEqual(self.hen_house.season, 'autumn')
 
     def test_productivity_index(self):
-        # mock the season method return with some correct season
-        # make sure _productivity_index returns correct value based on season and HenHouse.hens_productivity attribute
-        pass
+        with patch('hen_class.datetime.datetime') as e:
+            e.today().month = 1
+            self.hen_house.season
+            print(self.hen_house.season)
+            self.assertEqual(self.hen_house._productivity_index(),
+                             self.hen_house.hens_productivity['winter'])
 
     def test_productivity_index_incorrect_season(self):
-        # mock the season method return with some incorrect season
-        # make sure ErrorTimesOfYear is raised when _productivity_index called
-        pass
+        with patch('hen_class.datetime.datetime') as e:
+            e.today().month = -1
+            self.hen_house.season
 
-    def test_get_eggs_daily_in_winter(self):
-        # test get_eggs_daily function
-        # _productivity_index method or season should be mocked
-        pass
+            with self.assertRaises(ErrorTimesOfYear):
+                self.hen_house._productivity_index()
 
-    def test_get_max_count_for_soup(self):
-        # call get_max_count_for_soup with expected_eggs number and check that correct number is returned
+    @patch('hen_class.HenHouse._productivity_index', return_value=0.25)
+    def test_get_eggs_daily_in_winter(self, productivity_index):
+        self.assertEqual(self.hen_house.get_eggs_daily(self.hen_house.hen_count), 3)
 
-        # Note: make sure to mock _productivity_index or season
-        # in order not to call datetime.datetime.today().month, since it is going to be dynamic value in the future
-        pass
+    @patch('hen_class.HenHouse._productivity_index', return_value=0.25)
+    def test_get_max_count_for_soup(self, productivity_index):
+        self.assertEqual(self.hen_house.get_max_count_for_soup(1), 8)
 
-    def test_get_max_count_for_soup_returns_zero(self):
-        # call get_max_count_for_soup with expected_eggs number bigger than get_eggs_daily(self.hen_count)
-        # zero should be returned.
-
-        # Note: make sure to mock _productivity_index or season
-        # in order not to call datetime.datetime.today().month, since it is going to be dynamic value in the future
-        pass
+    @patch('hen_class.HenHouse._productivity_index', return_value=0.25)
+    def test_get_max_count_for_soup_returns_zero(self, productivity_index):
+        self.assertEqual(
+            self.hen_house.get_max_count_for_soup(self.hen_house.get_eggs_daily(self.hen_house.hen_count+10)), 0)
 
     def test_food_price(self):
-        # mock requests.get and make the result has status_code attr 200 and text to some needed value
-        # make sure food-price() return will be of int type
-        pass
+        with patch('hen_class.requests.get') as e:
+            e.return_value.status_code = 200
+            e.return_value.page_text = '12345678901234567890'
+
+            self.assertIsInstance(self.hen_house.food_price(), int)
 
     def test_food_price_connection_error(self):
-        # mock requests.get and make the result has status_code attr not 200
-        # check that ConnectionError is raised when food_price method called
-        pass
+        with patch('hen_class.requests.get') as e:
+            e.return_value.status_code = 404
+
+            with self.assertRaises(ConnectionError):
+                self.hen_house.food_price()
 
 
 if __name__ == '__main__':
